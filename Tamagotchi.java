b9e06b9f03e353dcb17683cfc4cc6b6c3cf2ea4d
James Merrington <merringtonjames@gmail.com>
2015-11-07 16:07:22 +0000

    adding a the updated Tamagotchi.java

--- Tamagotchi.java
+++ Tamagotchi.java
@@ -17,22 +17,48 @@ public class Tamagotchi {
 
 	public Tamagotchi(String name){
 		this.name = name;
+		sc = new Scanner(System.in);
+		menu();
 	}
 	
-	public void play(){
+	private void menu(){
+		int option = sc.nextInt();
+		switch(option){
+		case 1:	play();
+		case 2: sleep();
+		case 3: feed();
+		}
+	}
+	
+	private void play(){
 		System.out.println("You play with " + name);
+		if(playNeed < 200) playNeed += 800;
+		else playNeed = PLAY_CAP;
+		nextTurn();
+	}
+	
+	private void sleep(){
+		System.out.println("You put " + name + "to sleep");
 		if(sleepNeed < 200) sleepNeed += 800;
 		else sleepNeed = SLEEP_CAP;
 		nextTurn();
 	}
 	
-	public void sleep(){
-		System.out.println("You put " + name + "to sleep");
-		if(sleepNeed < 200) sleepNeed += 800;
-		else sleepNeed = SLEEP_CAP;
+	private void feed(){
+		System.out.println("You feed " + name);
+		if(hungerNeed < 200) hungerNeed += 800;
+		else hungerNeed = HUNGER_CAP;
+		nextTurn();
 	}
 	
 	private void nextTurn(){
-		
+		sleepNeed -= 100;
+		playNeed -= 150;
+		hungerNeed -= 200;
+		System.out.println("Current Stats (low is bad, high is good):");
+		System.out.println("Hunger: " + hungerNeed);
+		System.out.println("Sleep" + sleepNeed);
+		System.out.println("Fun need: " + playNeed);
+		menu();
 	}
 }
